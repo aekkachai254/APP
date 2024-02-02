14820b14e9b56f079312520051428aee3e32d532
aekkachai254 <98604806+aekkachai254@users.noreply.github.com>
2024-02-03 00:07:18 +0700

    Update Profile.dart
    update code

--- lib/screen/Profile.dart
+++ lib/screen/Profile.dart
@@ -1,10 +1,66 @@
-import 'package:application/screen/Home.dart';
+import 'package:application/screen/TravelList.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:application/screen/Home.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key});
+
+  Future<Map<String, dynamic>> _fetchUserProfile() async {
+    final response = await http.get(
+      Uri.parse(
+          'http://teamproject.ddns.net:81/project/api/profile.php?username=0640986563'),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> userProfile = json.decode(response.body);
+      return userProfile;
+    } else {
+      throw Exception('Failed to load user profile');
+    }
+  }
+
+  Future<String?> _fetchUserId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userId');
+  }
+
+  Future<void> _saveUserId(String userId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('userId', userId);
+  }
+
+  Future<void> _saveUserPhoneNumber(String phoneNumber) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('userPhoneNumber', phoneNumber);
+  }
+
+  Future<String?> _fetchUserPhoneNumber() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userPhoneNumber');
+  }
+
+  Future<void> _saveUserEmail(String email) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('userEmail', email);
+  }
+
+  Future<String?> _fetchUserEmail() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userEmail');
+  }
+
+  Future<void> _saveUserBirthDate(String birthDate) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('userBirthDate', birthDate);
+  }
+
+  Future<String?> _fetchUserBirthDate() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userBirthDate');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +82,7 @@ class ProfileScreen extends StatelessWidget {
           },
         ),
         title: const Text(
-          "ผู้ใช้",
+          "ข้อมูลพนักงาน",
           style: TextStyle(
             color: Colors.white,
           ),
@@ -42,8 +98,8 @@ class ProfileScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 const CircleAvatar(
-                  radius: 65,
-                  backgroundColor: Colors.white,
+                  radius: 60,
+                  backgroundColor: Color(0xFF0464F5),
                   child: CircleAvatar(
                     backgroundImage: AssetImage("assets/images/Profile.png"),
                     radius: 55,
@@ -54,50 +110,129 @@ class ProfileScreen extends StatelessWidget {
                   "วิศุทธิ์ พรนิมิตร",
                   style: TextStyle(fontSize: 28, color: Colors.white),
                 ),
-                // แสดง ID จากข้อมูลผู้ใช้
-                FutureBuilder<List<Map<String, dynamic>>>(
-                  future: _fetchAllUserData(),
+                FutureBuilder<Map<String, dynamic>>(
+                  future: _fetchUserProfile(),
                   builder: (context, snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return CircularProgressIndicator();
                     } else if (snapshot.hasError) {
                       return Text('Error: ${snapshot.error}');
                     } else {
+                      String id = snapshot.data?['id'].toString() ?? "";
+                      String firstname =
+                          snapshot.data?['firtsname'].toString() ?? "";
+
                       return Column(
-                        children: snapshot.data!.map((userData) {
-                          return Text(
-                            'ID: ${userData['id'] ?? ""}',
+                        children: [
+                          const Text(
+                            "ID :",
                             style: TextStyle(fontSize: 18, color: Colors.white),
-                          );
-                        }).toList(),
+                          ),
+                          Text(
+                            id,
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const ListTile(
+                            leading: Icon(
+                              Icons.person,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                            subtitle: Text(
+                              "แผนก",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            title: Text(
+                              "ผู้จัดการ",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const ListTile(
+                            leading: Icon(
+                              Icons.cake,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                            subtitle: Text(
+                              "วันเกิด",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            title: Text(
+                              "16 มีนาคม 2566",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const ListTile(
+                            leading: Icon(
+                              Icons.email,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                            subtitle: Text(
+                              "อีเมล",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            title: Text(
+                              "wisutphonnimit@gmail.com",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const ListTile(
+                            leading: Icon(
+                              Icons.phone,
+                              size: 30,
+                              color: Colors.white,
+                            ),
+                            subtitle: Text(
+                              "หมายเลขโทรศัพท์",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            title: Text(
+                              "098-758-9631",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
                       );
                     }
                   },
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                // ... ส่วนอื่น ๆ ของ Widget
                 InkWell(
                   onTap: () async {
-                    final response = await http.get(Uri.parse(
-                        'http://teamproject.ddns.net:81/project/api/profile.php?username=0640986563'));
+                    try {
+                      final Map<String, dynamic> userProfile =
+                          await _fetchUserProfile();
+                      final String id = userProfile['id'].toString();
+                      final String firstname =
+                          userProfile['firstname'].toString();
 
-                    if (response.statusCode == 200) {
-                      List<Map<String, dynamic>> userProfiles =
-                          List<Map<String, dynamic>>.from(
-                              json.decode(response.body));
+                      print('ID: $id');
+                      print('firstname: $firstname');
+
+                      await _saveUserId(id);
 
                       Navigator.push(
                         context,
                         MaterialPageRoute(
                           builder: (context) => ProfileDetailScreen(
-                            userProfiles: userProfiles,
+                            userProfiles: [userProfile],
                           ),
                         ),
                       );
-                    } else {
-                      print('Failed to load user data');
+                    } catch (e) {
+                      print('Error: $e');
                     }
                   },
                   child: Expanded(
@@ -106,7 +241,7 @@ class ProfileScreen extends StatelessWidget {
                       child: Container(
                         width: 250.0,
                         decoration: BoxDecoration(
-                          color: const Color(0xFF567BAE),
+                          color: Color(0xFF0D99FF),
                           borderRadius: BorderRadius.circular(15.0),
                           border: Border.all(color: Colors.white, width: 1.0),
                         ),
@@ -138,20 +273,6 @@ class ProfileScreen extends StatelessWidget {
       ),
     );
   }
-
-  // เพิ่ม Future function เพื่อดึงข้อมูลผู้ใช้ทั้งหมดจาก API
-  Future<List<Map<String, dynamic>>> _fetchAllUserData() async {
-    final response = await http.get(Uri.parse(
-        'http://teamproject.ddns.net:81/project/api/profile.php?username=0640986563'));
-
-    if (response.statusCode == 200) {
-      List<Map<String, dynamic>> userProfiles =
-          List<Map<String, dynamic>>.from(json.decode(response.body));
-      return userProfiles;
-    } else {
-      throw Exception('Failed to load user data');
-    }
-  }
 }
 
 class ProfileDetailScreen extends StatelessWidget {
@@ -164,20 +285,145 @@ class ProfileDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('รายละเอียดผู้ใช้'),
+        title: Text('รายละเอียดพนักงาน'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: userProfiles.map((userData) {
-            return Column(
-              children: [
-                Text('ID: ${userData['id']}'),
-                Text('Name: ${userData['name']}'),
-                // แสดงข้อมูลอื่น ๆ ตามที่ต้องการ
-              ],
+          children: <Widget>[
+            Text('รายละเอียดของพนักงาน:'),
+            for (var userProfile in userProfiles)
+              Text('${userProfile['id']} - ${userProfile['name']}'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.black38,
+        onDestinationSelected: (index) {
+          if (index == 1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ProfileScreen(),
+              ),
             );
-          }).toList(),
+          }
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "หน้าหลัก"),
+          NavigationDestination(icon: Icon(Icons.person), label: "ผู้ใช้"),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                    image: AssetImage("assets/images/app_logo.png"),
+                    height: 300,
+                    width: 300,
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const TravelListScreen(),
+                            ),
+                          );
+                        },
+                        child: const Image(
+                          image: AssetImage("assets/images/truck.png"),
+                          color: Colors.white,
+                          height: 90,
+                          width: 90,
+                        ),
+                      ),
+                      const Text(
+                        "รายการ",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        "เดินทาง",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 30),
+                  Column(
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: const Image(
+                          image: AssetImage("assets/images/qr.png"),
+                          color: Colors.white,
+                          height: 70,
+                          width: 70,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "รายละเอียด",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        "สินค้า",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
